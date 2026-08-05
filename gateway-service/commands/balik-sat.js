f83e121d1c9609f70ac955c5dccd24c3cc7e07ad
coniconiindirdoni-cell <coniconiindirdoni@gmail.com>
2026-08-05 16:37:58 +0300

    Update import paths in balik-sat.js

--- gateway-service/commands/balik-sat.js
+++ gateway-service/commands/balik-sat.js
@@ -1,7 +1,7 @@
 // commands/rpg/slot.js
 const { SlashCommandBuilder } = require('discord.js');
-const { gameCore } = require('../../lib/service-clients');
-const { safeExecute, friendlyError } = require('../../lib/safe-execute');
+const { gameCore } = require('../lib/service-clients');
+const { safeExecute, friendlyError } = require('../lib/safe-execute');
 
 module.exports = {
   data: new SlashCommandBuilder()
