7aa70a6b59a0686d7f4f5990d7c024619270406b
coniconiindirdoni-cell <coniconiindirdoni@gmail.com>
2026-08-05 13:20:48 +0300

    Add files via upload

---
+++ database-service/index.js
@@ -0,0 +1,16 @@
+{
+  "name": "database-service",
+  "version": "2.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "check": "node --check index.js"
+  },
+  "engines": {
+    "node": ">=20"
+  },
+  "dependencies": {
+    "express": "^4.21.2",
+    "pg": "^8.13.3"
+  }
+}
