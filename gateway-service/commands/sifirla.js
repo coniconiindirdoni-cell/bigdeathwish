e03c6a4ccb2fc9bc6201d40faa596341e3fc4a19
coniconiindirdoni-cell <coniconiindirdoni@gmail.com>
2026-08-05 16:47:40 +0300

    Update sifirla.js

--- gateway-service/commands/sifirla.js
+++ gateway-service/commands/sifirla.js
@@ -1,7 +1,7 @@
 // commands/user/sohbet.js
 const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
-const { user } = require('../../lib/service-clients');
-const { safeExecute, friendlyError } = require('../../lib/safe-execute');
+const { user } = require('../lib/service-clients');
+const { safeExecute, friendlyError } = require('../lib/safe-execute');
 
 module.exports = {
   data: new SlashCommandBuilder()
