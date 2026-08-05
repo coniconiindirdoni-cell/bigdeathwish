4afb688534b8c8224a573ed91b587cbacb90a8dc
coniconiindirdoni-cell <coniconiindirdoni@gmail.com>
2026-08-05 16:45:53 +0300

    Update renkrolekle.js

--- gateway-service/commands/renkrolekle.js
+++ gateway-service/commands/renkrolekle.js
@@ -1,7 +1,7 @@
 // commands/economy/market-yonet.js
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const { economy } = require('../../lib/service-clients');
-const { safeExecute, friendlyError } = require('../../lib/safe-execute');
+const { economy } = require('../lib/service-clients');
+const { safeExecute, friendlyError } = require('../lib/safe-execute');
 
 module.exports = {
   data: new SlashCommandBuilder()
